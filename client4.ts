ba9776574a1174e81699db7e99159074ffab1e82
Yuttasak (Yo) Pannawat <yuttasakyo@gmail.com>
2020-02-24 12:33:34 +0700

    Created Server

--- client4.ts
+++ client4.ts
@@ -1,8 +1,7 @@
 import io from 'socket.io-client'
+
 const socket = io('ws://localhost:3000')
 
 const EVENT = 'ASSIGN_TASK'
 socket.emit('JOIN', { event: EVENT })
-socket.on(`${EVENT}_SUBSCRIBE`, (msg: any) => {
-  console.log(msg)
-})
+socket.emit('SEND_MESSAGE', { event: EVENT, name: 'Yo', status: 'COMPLETED' }) // FAILED
